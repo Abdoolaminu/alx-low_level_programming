1d55de64ce7fd2c0249b1ed96318110e23fea37b
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2022-10-13 08:23:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
- * main - Print a text
- *
- * Reurn: Always 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
