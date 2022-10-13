0cf7959b9d9c5381458d2c35d597ce3267a5a40c
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2022-10-13 08:19:32 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main -Entry
- * Reurn: Always 0
-*/
+ * main - Print a text
+ *
+ * Reurn: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
