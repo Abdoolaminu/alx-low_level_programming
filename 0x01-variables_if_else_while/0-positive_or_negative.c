dfb2ccfe38a6314542a6c088a53f493048dfac35
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2022-10-14 09:01:11 -0700

    Number is +ve or -ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,17 @@
-#include<stdio.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - determine if number is positive or negative
- *
- * Return: Always 0 (Success)
+ * main - entry point
+ * Description: prints if a number is positive or negative.
+ * Return: 0
  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2
+	n = rand() - RAND MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -19,9 +20,10 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-	else
+	else if (n < 0)
 	{
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
+
