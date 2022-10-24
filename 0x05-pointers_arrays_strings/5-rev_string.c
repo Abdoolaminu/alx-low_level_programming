745b3e8d7615a23de009e02418084a8028f1a3ea
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2022-10-24 09:11:42 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,17 +12,15 @@ void rev_string(char *s)
 
 		len = 0;
 		len1 = 0;
-
 		while (s[len] != '\0')
 		{
-			len++;
+		len++;
 		}
 		len1 = len - 1;
 		for (i = 0; i < len / 2; i++)
 		{
-			tmp = s[i];
-			[i] = s[len1];
-			s[len1--] = tmp;
-		}
+		tmp = s[i];
+		s[i] = s[len1];																s[len1--] = tmp;
+																		}
 }
 
