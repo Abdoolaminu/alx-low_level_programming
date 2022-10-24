83f45f94bd63d65e0b5fe44921499aa4320b4481
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2022-10-24 09:28:59 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,91 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string
- * @s: string to be reversed
- */
+ *
+ * *rev_string - Reverse a string
+ *
+ * *@s: String to reverse
+ *
+ * *Return: Nothing
+ *
+ * */
+
 void rev_string(char *s)
+
 {
-		char tmp;
 
-		int i, len, len1;
+	int i = 0, length;
 
-		len = 0;
-		len1 = 0;
-		while (s[len] != '\0')
-		{
-		len++;
-		}
-		len1 = len - 1;
-		for (i = 0; i < len / 2; i++)
-		{
-		tmp = s[i];
-		s[i] = s[len1];																s[len1--] = tmp;
-																		}
+	length = _strlen(s) - 1;
+
+	while (length > i)
+
+	{
+
+		swap_char(s + length, s + i);
+
+		i++;
+
+		length--;
+
+	}
+
 }
 
+
+
+/**
+ *
+ *  *_strlen - returns the length of a string
+ *
+ *   * @s: string
+ *
+ *    *Return: returns lenght;
+ *
+ *     */
+
+int _strlen(char *s)
+
+{
+
+	int count, inc;
+
+	inc = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+
+		inc++;
+
+
+
+	return (inc);
+
+}
+
+
+
+/**
+ *
+ * *swap_char - swap two characters
+ *
+ * *@a: first character
+ *
+ * *@b: second character
+ *
+ * *Return: nothing
+ *
+ * */
+
+
+
+void swap_char(char *a, char *b)
+
+{
+
+	char tmp = *a;
+
+	*a = *b;
+
+	*b = tmp;
+
+}
