8f49edc83e752f5c8e9f059dc40cfca827edbe0a
Abdoolaminu <abdurrahmanaminu2@gmail.com>
2023-01-16 06:26:38 -0800

    0-main.c

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -6,20 +6,20 @@
 
 /**
  *
- *  * main - check the code
+ * main - check the code
  *
- *   *
+ * Return: Always EXIT_SUCCESS.
  *
- *    * Return: Always EXIT_SUCCESS.
- *
- *     */
+ */
 
 int main(void)
 
 {
 
-	    printf("%d\n", _strlen("My Dyn Lib"));
+	printf("%d\n", _strlen("My Dyn Lib"));
 
-	        return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 
 }
+
+
